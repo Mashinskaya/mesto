0ffd9ce6eb8de61444f0382c0c52a129a004f41a
Marina Mashinskaya <yama10@yandex.ru>
2021-03-24 18:05:55 +0300

    пофиксить сабмит

---
+++ scripts/scripts.js
@@ -0,0 +1,43 @@
+const popup = document.querySelector('.popup');
+
+const openPopupBtn = document.getElementById('open_popup_btn');
+
+const closePopupBtn = document.querySelector('.popup__close-button');
+const safePopupBtn = document.querySelector('.popup__safe-button');
+
+function openPopup() {
+  popup.classList.add('popup__visible')
+}
+function closePopup() {
+  popup.classList.remove('popup__visible')
+}
+
+openPopupBtn.addEventListener('click', function() {
+  openPopup()
+});
+
+closePopupBtn.addEventListener('click', function() {
+  closePopup()
+});
+
+safePopupBtn.addEventListener('click', function() {
+  closePopup()
+});
+
+/*
+
+let formElement = document.querySelector('.popup__content');
+
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__job');
+
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  nameInput.textContent = nameInput.value;
+  jobInput.textContent = jobInput.value;
+  }
+
+formElement.addEventListener('submit', formSubmitHandler); 
+*/
