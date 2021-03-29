02741b8ce39071ec3c31e98ee05983c61e669bfc
Marina Mashinskaya <yama10@yandex.ru>
2021-03-29 18:11:30 +0300

    after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 let popup = document.querySelector('.popup');
 
-let openPopupBtn = document.getElementById('open_popup_btn');
+let openPopupBtn = document.querySelector('.open_popup_btn');
 
 let closePopupBtn = document.querySelector('.popup__close-button');
 let savePopupBtn = document.querySelector('.popup__save-button');
+
 
 function openPopup() {
   popup.classList.add('popup__visible')
@@ -12,28 +13,28 @@ function closePopup() {
   popup.classList.remove('popup__visible')
 }
 
-openPopupBtn.addEventListener('click', function() {
-  openPopup()
-});
+openPopupBtn.addEventListener('click', openPopup) 
 
-closePopupBtn.addEventListener('click', function() {
-  closePopup()
-});
 
-savePopupBtn.addEventListener('click', function() {
-  closePopup()
-});
+closePopupBtn.addEventListener('click', closePopup)
+
+
+
+
+
 
 let formElement = document.querySelector('.popup__content');
 let nameInput = document.querySelector('.profile__title');
 let jobInput = document.querySelector('.profile__subtitle');
 let nameElement = document.querySelector('.popup__name');
 let jobElement = document.querySelector('.popup__job');
+console.log(popup__name.textContent);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameInput.textContent = nameElement.value;
   jobInput.textContent = jobElement.value;
+  closePopup()
 };
 
 formElement.addEventListener('submit', formSubmitHandler); 
